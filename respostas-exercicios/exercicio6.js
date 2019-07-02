f9d4f4e7404989d9b89704ecd82392eeecd950cd
Mateus <mateus_sat@hotmail.com>
2019-07-02 19:46:14 -0300

    comentarios

--- respostas-exercicios/exercicio6.js
+++ respostas-exercicios/exercicio6.js
@@ -1,3 +1,6 @@
 let raio = prompt('Entre com o raio do circulo: ');
 let area = (Math.PI * raio ** 2);
 alert('A area é: ' + area);
+
+//constantes deve ser maiusculas(apenas por questão de boa pratica);
+//Math.PI é uma constante de PI do js. ele retorna o valor de PI;
