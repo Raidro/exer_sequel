664f5caacf0fa6374f1cdfd417db0d3e7e96a14b
Mateus <mateus_sat@hotmail.com>
2019-07-02 20:41:12 -0300

    ajustes finos

--- respostas-exercicios/exercicio6.js
+++ respostas-exercicios/exercicio6.js
@@ -1,4 +1,4 @@
-let raio = prompt('Entre com o raio do circulo: ');
+let raio = parseFloat(prompt('Entre com o raio do circulo: '));
 let area = (Math.PI * raio ** 2);
 alert('A area é: ' + area);
 
