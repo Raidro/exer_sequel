86a90c059fa439f2e1fa3a8f6360ec4b79eb7e35
Mateus <mateus_sat@hotmail.com>
2019-07-02 19:52:23 -0300

    trocando a area para a area do quadrado

--- respostas-exercicios/exercicio7.js
+++ respostas-exercicios/exercicio7.js
@@ -1,5 +1,5 @@
-let base = prompt('Entre com o valor da base do quadrado: ');
-let altura = prompt('Entre com o valor da altura do quadrado: ');
+let lado = prompt('Entre com o valor do Lado do quadrado: ');
+//let altura = prompt('Entre com o valor da altura do quadrado: ');
 
-let area = base * altura;
+let area = lado * lado;//* altura;
 alert('A area é: ' + area + '\n\n' + 'E o dobro é: ' + area * 2);
