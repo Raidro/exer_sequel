3b6fa9835b1eec93aa9a1d5ce84da992446a962d
Mateus <mateus_sat@hotmail.com>
2019-07-02 19:57:17 -0300

    modificando os nomes para serem mais legiveis

--- respostas-exercicios/exercicio8.js
+++ respostas-exercicios/exercicio8.js
@@ -1,5 +1,5 @@
-let gh = prompt('Quanto você ganha por hora? ');
-let hm = prompt('quantas horas você trabalhou no mês? ');
+let ganhoHora = prompt('Quanto você ganha por hora? ');
+let horaMes = prompt('quantas horas você trabalhou no mês? ');
 
-let valor = gh * hm;
-alert('Você Ganha: R$ '+valor);
+let valor = ganhoHora * horaMes;
+alert('Você Ganha: R$ ' + valor);
